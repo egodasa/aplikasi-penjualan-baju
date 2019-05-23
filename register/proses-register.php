93dd788e6baee5550700bb13263181209d47cadb
Ego Dafma Dasa <egodasa@gmail.com>
2019-05-23 09:23:25 +0700

    Halaman register belum jalan

---
+++ register/proses-register.php
@@ -0,0 +1,25 @@
+<?
+  session_start();
+  require('../pengaturan/helper.php');
+  require('../vendor/autoload.php');
+  require('../pengaturan/medoo.php');
+  
+  $cek_username = $db->get("pengguna", ['username'], ['username' => $_POST['username']]);
+  
+  if(!empty($cek_username))
+  {
+    $db->insert("pengguna", [
+      'username' => $_POST['username'],
+      'nm_pengguna' => $_POST['nm_pengguna'],
+      'password' => $_POST['password'],
+      'jenis_pengguna' => "Pelanggan",
+      'nohp' => $_POST['nohp'],
+      'alamat' => $_POST['alamat']
+    ]);
+    header("Location: ".$alamat_web."/login?daftar=true");
+  }
+  else
+  {
+    header("Location: ".$alamat_web."/register?username=salah");
+  }
+?>
